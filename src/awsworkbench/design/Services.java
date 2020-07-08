95a18adfb04b26ae9c3cb7028d81c781104b4ac8
RKVS Raman <rkvsraman@gmail.com>
2020-07-08 23:44:29 +0530

    fixed list vies

--- src/awsworkbench/design/Services.java
+++ src/awsworkbench/design/Services.java
@@ -1,6 +1,7 @@
 package awsworkbench.design;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
@@ -20,17 +21,37 @@ public class Services {
 	 * http://help.eclipse.org/neon/index.jsp?topic=%2Forg.eclipse.sirius.doc%2Fdoc%2Findex.html&cp=24
 	 * for documentation on how to write service methods.
 	 */
-	public Collection<String> myService(EObject self, EStructuralFeature feature) {
-		// TODO Auto-generated code
+	public Collection<String> getPropertyValueStringAsCollection(EObject self, EStructuralFeature feature) {
+		
+		
+		if(self.eGet(feature) == null || self.eGet(feature).toString().isEmpty())
+		{
+			return new ArrayList<String>();
+		}
 
 		String[] arr = self.eGet(feature).toString().split(",");
 		ArrayList<String> arrL = new ArrayList<String>();
 
 		for (String s : arr) {
-			arrL.add(s);
+			arrL.add(s.trim());
 		}
 
 		return arrL;
+	}
+	
+	public EObject removeValue(EObject self, EStructuralFeature feature, Object value) {
+		
+		
+		Collection<String> selectedValues = (Collection<String>) value;
+		if(self.eGet(feature) != null || !self.eGet(feature).toString().isEmpty())
+		{
+			List<String> existing = Arrays.asList(self.eGet(feature).toString().split(","));
+			existing.removeAll(selectedValues);
+			self.eSet(feature, String.join(",", existing));
+		}
+	
+		
+		return self;
 	}
 
 }

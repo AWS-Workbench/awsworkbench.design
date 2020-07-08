5c827a1d0cc50be6da2381a01bfbf233cf452a67
RKVS Raman <rkvsraman@gmail.com>
2020-07-08 14:10:50 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# awsworkbench.design
---
+++ src/awsworkbench/design/Services.java
@@ -0,0 +1,36 @@
+package awsworkbench.design;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.impl.EStringToStringMapEntryImpl;
+
+/**
+ * The services class used by VSM.
+ */
+public class Services {
+
+	/**
+	 * See
+	 * http://help.eclipse.org/neon/index.jsp?topic=%2Forg.eclipse.sirius.doc%2Fdoc%2Findex.html&cp=24
+	 * for documentation on how to write service methods.
+	 */
+	public Collection<String> myService(EObject self, EStructuralFeature feature) {
+		// TODO Auto-generated code
+
+		String[] arr = self.eGet(feature).toString().split(",");
+		ArrayList<String> arrL = new ArrayList<String>();
+
+		for (String s : arr) {
+			arrL.add(s);
+		}
+
+		return arrL;
+	}
+
+}
